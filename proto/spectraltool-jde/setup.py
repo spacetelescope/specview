eed63406da62b00241669ef80f2836a782b26ec6
nmearl <nchlsearl@gmail.com>
2015-02-19 16:15:37 -0500

    Rolling back; want to maintain history

--- proto/spectraltool-jde/setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(1, '../spec-widgets/specviewer')
-from sp_model_manager import ModelManager
-from specviewer import SpecViewer
-
-mm = ModelManager()
-
-sv = SpecViewer()

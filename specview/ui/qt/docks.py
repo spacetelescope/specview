1fcc357d9608bea2181de0a9a7f85a98a59398ae
ibusko <busko@stsci.edu>
2015-05-14 15:03:23 -0400

    Fix broken code. Test button should be visible for now.

--- specview/ui/qt/docks.py
+++ specview/ui/qt/docks.py
@@ -220,7 +220,7 @@ class ModelDockWidget(BaseDockWidget):
         #
         # TODO for testing only. Must be replaced by appropriate signal/slot
         #
-        # self.btn_replot_model = QtGui.QPushButton("&Re-plot Model")
+        self.btn_replot_model = QtGui.QPushButton("&Re-plot Model")
         #
 
         # self.add_layout(hb_layout)
@@ -229,6 +229,6 @@ class ModelDockWidget(BaseDockWidget):
         self.add_widget(self.wgt_fit_selector)
         self.add_widget(self.btn_perform_fit)
         #
-        # self.add_widget(self.btn_replot_model)
+        self.add_widget(self.btn_replot_model)
         #
         self.setMinimumSize(self.sizeHint())

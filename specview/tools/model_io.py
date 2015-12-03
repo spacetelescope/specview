14ac4de1daafe5e1b1959f00e6c0c64efebfa0a1
ibusko <busko@stsci.edu>
2015-12-03 08:57:44 -0500

    Add supporting code for enabling reading/saving spectral models to files.

---
+++ specview/tools/model_io.py
@@ -0,0 +1,189 @@
+#
+# Functions in this module support the reading and writing
+# of astropy's compound spectral models from/to file.
+#
+# The format used in these files for now is plain python,
+# directly importable by the user. This format was introduced
+# and is discussed in the specfit project at:
+#
+# https://github.com/ibusko/specfit
+#
+
+import os, sys, re, dis
+
+from cStringIO import StringIO
+from PyQt4.QtGui import QFileDialog
+
+import model_registry
+
+
+# Builds a compound model specified in a .py file
+def buildModelFromFile(fname):
+    directory = os.path.dirname(str(fname))
+    sys.path.append(directory)
+
+    f = os.path.basename(str(fname)).split('.')[0] # remove .py from end of file name so it can be imported
+    import_statement = "import " + f + " as module"
+
+    try:
+        exec import_statement in locals(), locals()
+
+        # this will pick up the first model defined in the file. A different
+        # mechanism is needed to handle files with multiple model definitions.
+        for variable in dir(module):
+            if not variable.startswith('__'):
+                # this assumes that the module contains only model definitions and
+                # imports for the functional types used in the model definitions.
+                # This 'if' statement skips the function types, everything that
+                # passes is assumed to be a valid compound model definition.
+                if (str(type(module.__dict__[variable]))).find('astropy.modeling.core._ModelMeta') < 0:
+                    compound_model = module.__dict__[variable]
+                    return compound_model, directory
+        return None,None
+    except Exception as e:
+        print("ERROR: " + e)
+        return None,None
+
+
+# Builds a model expression inside a string, and dumps string to file.
+def saveModelToFile(parent, model, model_directory):
+
+    # The following assumes that the formatted string expression
+    # in an astropy compound model has operands of the form [0], [1],
+    # etc, that is, a sequential number enclosed in square brackets.
+    expression = model.compound_model._format_expression()
+    tokens = re.split(r'[0-9]+', expression)
+
+    # this loop builds the main expression, and captures
+    # information needed for building the file header (where
+    # the import statements go).
+    expression_string = ""
+    import_module_names = {}
+    for token, component in zip(tokens, model.items):
+        # clean up astropy-inserted characters
+        token = token.replace('[','')
+        token = token.replace(']','')
+
+        expression_string += str(token) + assemble_component_spec(component)
+
+        # here we store the module paths for each component. Using
+        # a dictionary key ensures that we get only one of each.
+        path = model_registry.get_component_path(component)
+        name = model_registry.get_component_name(component)
+        import_module_names[name] = path
+
+    # this loop now uses the captured information from above to
+    # build a set of import statements that go at the beginning
+    # of the file.
+    prolog = ""
+    for name, path in import_module_names.iteritems():
+        prolog += "from " + path + " import " + name + "\n"
+    prolog += "\n"
+    # we need to add a reference to the model so it can actually
+    # be used after imported. We just use 'model1' for the variable
+    # name. This also implicitly assumes that only one model will be
+    # stored in the file. It remains to be seen how useful this
+    # assumption will be in practice.
+    prolog += "model1 = \\\n"
+
+    # Write to file.
+    fname = QFileDialog.getSaveFileName(parent, 'Write to file', model_directory)
+
+    if len(fname) > 0:
+        f = os.open(fname, os.O_RDWR|os.O_CREAT)
+        os.write(f, prolog)
+        os.write(f, expression_string)
+        os.close(f)
+
+
+# Disassembles a tie callable. Ties read from a model
+# file are not directly accessible in text form because
+# the model file is compiled at import time.
+def get_tie_text(tie):
+    if tie:
+        # dis() only outputs on standard output.....
+        keep = sys.stdout
+        sys.stdout = StringIO()
+        dis.dis(tie)
+        assembler_text = sys.stdout.getvalue()
+        sys.stdout.close()
+        sys.stdout = keep
+        result = _parse_assembler_text(assembler_text)
+    else:
+        result = 'False'
+    return result
+
+
+# This parses the text returned by the disassembler for
+# a lambda function that multiplies a constant by a
+# variable. That is, we are assuming that ties are coded
+# as lambda functions with multiplication by a constant,
+# as in STSDAS' specfit.
+parser = re.compile(r'\(([^)]+)\)') # picks up whatever is enclosed in parenthesis
+def _parse_assembler_text(text):
+    tokens = parser.findall(text)
+    factor = tokens[0]
+    lambda_variable_name = tokens[1]
+    function_id = tokens[2]
+    par_name = tokens[3]
+
+    return "lambda %s: %s *  %s[%s].%s" % \
+           (lambda_variable_name,
+            factor,
+            lambda_variable_name,
+            function_id,
+            par_name)
+
+
+# Builds an operand (a spectral component) for an astropy compound model.
+def assemble_component_spec(component):
+    result = ""
+
+    # function name - Note that get_component_name works
+    # pretty much independently of the models registry.
+    # Any model will work because the function name is
+    # derived from the component's __class__.
+    name = model_registry.get_component_name(component)
+    result += name
+
+    # component name
+    if component.name:
+        result += "(name = \'"
+        result += component.name + "\',\n"
+    else:
+        result += "(\n"
+
+    # parameter names and values
+    for i, param_name in enumerate(component.param_names):
+        result += "            " + param_name
+        result += " = "
+        result += str(component.parameters[i]) + ",\n"
+
+    # parameter bounds
+    bounds = component.bounds
+    result += "            bounds = {\n"
+    for param_name in component.param_names:
+        result += "                     '" + param_name + "': " + str(bounds[param_name]) + ",\n"
+    result += "                     },\n"
+
+    # parameter fixed flags
+    fixed = component.fixed
+    result += "            fixed = {\n"
+    for param_name in component.param_names:
+        result += "                     '" + param_name + "': " + str(fixed[param_name]) + ",\n"
+    result += "                    },\n"
+
+    # parameter ties. Ties have to be disassembled and parsed
+    # in order to become human-readable and writable to file.
+    ties = component.tied
+    result += "            tied = {\n"
+    for param_name in component.param_names:
+        tie_text = get_tie_text(ties[param_name])
+        result += "                    '" + param_name + "': " + tie_text + ",\n"
+    result += "                   },\n"
+
+    result += "            ) \\\n"
+    return result
+
+
+
---
+++ specview/tools/model_registry.py
@@ -0,0 +1,44 @@
+#
+# astropy spectral models
+#
+# this file should (perhaps?) be replaced by some form of
+# introspection from the astropy.modeling.Model registry.
+# For now, we directly store hard-coded instances.
+
+import astropy.modeling.models as models
+
+registry = {
+    'Box1D' :                     models.Box1D(1.0, 1.0, 1.0),
+    'Gaussian1D':                 models.Gaussian1D(1.0, 1.0, 1.0),
+    'GaussianAbsorption1D':       models.GaussianAbsorption1D(1.0, 1.0, 1.0),
+    'Lorentz1D':                  models.Lorentz1D(1.0, 1.0, 1.0),
+    'MexicanHat1D':               models.MexicanHat1D(1.0, 1.0, 1.0),
+    'Trapezoid1D':                models.Trapezoid1D(1.0, 1.0, 1.0, 1.0),
+    'ExponentialCutoffPowerLaw1D':models.ExponentialCutoffPowerLaw1D(1.0, 1.0, 1.0, 1.0),
+    'BrokenPowerLaw1D':           models.BrokenPowerLaw1D(1.0, 1.0, 1.0, 1.0),
+    'LogParabola1D' :             models.LogParabola1D(1.0, 1.0, 1.0, 1.0),
+    'PowerLaw1D' :                models.PowerLaw1D(1.0, 1.0, 1.0),
+    'Linear1D':                   models.Linear1D(1.0, 0.0),
+    'Const1D':                    models.Const1D(0.0),
+    'Redshift':                   models.Redshift(0.0),
+    'Scale':                      models.Scale(1.0),
+    'Shift':                      models.Shift(0.0),
+    'Sine1D' :                    models.Sine1D(1.0, 1.0),
+    'Chebyshev1D':                models.Chebyshev1D(1),
+    'Legendre1D' :                models.Legendre1D(1),
+    'Polynomial1D' :              models.Polynomial1D(1),
+}
+
+
+def get_component_name(function):
+    # there must be a better way of getting the class' name......
+    class_string = str(function.__class__)
+    return class_string.split('\'>')[0].split(".")[-1]
+
+
+def get_component_path(function):
+    class_string = str(function.__class__)
+    module_path = class_string.split('\'')[1]
+    index = module_path.rfind('.')
+    module_path = module_path[:index]
+    return module_path

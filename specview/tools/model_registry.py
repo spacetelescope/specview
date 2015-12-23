6b73856fa776299279917d3367f85f5a048fa374
ibusko <busko@stsci.edu>
2015-12-23 15:01:03 -0500

    Comment about astropy bug.

--- specview/tools/model_registry.py
+++ specview/tools/model_registry.py
@@ -62,6 +62,22 @@ def buildSummedCompoundModel(components):
 
 def getComponents(compound_model):
     # build a list of components from a compound model
+
+    #TODO
+    #TODO   the simple construct below has a nasty behavior caused by a
+    #TODO   bug in astropy. When we submit a compound model to a fitter,
+    #TODO   such as in:
+    #TODO
+    #TODO   fitted_model = fitting.LevMarLSQFitter()(initial_model, x, y)
+    #TODO
+    #TODO   the fit results are used to populate the parameter values of each
+    #TODO   component in the fitted_model compound model. These new parameeter
+    #TODO   values are used everywhere, *except* to update the _submodels list
+    #TODO   in the compound model. The resulting fitted_model is thus internally
+    #TODO   inconsistent. Thus, if we use _submodels as a source for the new
+    #TODO   values, we will never get those, but will keep getting the original
+    #TODO   values used when the compound model was built.
+
     if hasattr(compound_model, '_submodels'):
         return compound_model._submodels
     else:
